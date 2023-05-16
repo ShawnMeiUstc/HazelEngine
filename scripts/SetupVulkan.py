1f9289ea812eedf035ac157a0ff6bd183626bd88
Shawn_Mei <mrmei1991@163.com>
2023-05-16 16:18:36 +0800

    setup things

--- scripts/SetupVulkan.py
+++ scripts/SetupVulkan.py
@@ -9,8 +9,8 @@ from io import BytesIO
 from urllib.request import urlopen
 
 class VulkanConfiguration:
-    requiredVulkanVersion = "1.3."
-    installVulkanVersion = "1.3.216.0"
+    requiredVulkanVersion = "1.3.246.1"
+    installVulkanVersion = "1.3.246.1"
     vulkanDirectory = "./Hazel/vendor/VulkanSDK"
 
     @classmethod
